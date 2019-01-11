fae599cafee6ee25309082136117f31e6b696193
klimson <klimson@gitlab.com>
2019-01-11 22:04:11 +0100

    Switch pattern

--- src/org/blog/parser/PropertiesParser.java
+++ src/org/blog/parser/PropertiesParser.java
@@ -2,31 +2,76 @@ package org.blog.parser;
 
 import org.blog.Properties;
 import org.blog.PropertiesKey;
-import org.blog.parser.strategy.PropertyBaseUrlParserStrategy;
-import org.blog.parser.strategy.PropertyParserStrategy;
-import org.blog.parser.strategy.PropertyTimeoutParserStrategy;
 
-import java.util.EnumMap;
 import java.util.List;
 
 public class PropertiesParser {
-    private final EnumMap<PropertiesKey, PropertyParserStrategy> parserStrategies = new EnumMap<>(PropertiesKey.class);
-
-    public PropertiesParser() {
-        parserStrategies.put(PropertiesKey.BASE_URL, new PropertyBaseUrlParserStrategy());
-        parserStrategies.put(PropertiesKey.TIMEOUT, new PropertyTimeoutParserStrategy());
-        parserStrategies.put(PropertiesKey.PORT, new PropertyTimeoutParserStrategy());
-    }
-
     public Properties parse(List<String> lines) {
         Properties properties = new Properties();
 
         for (String line : lines) {
-            String [] values = line.split("=");
-            String key = values[0];
-            String value = values[1];
+            properties = parse(properties, line);
 
-            properties = parserStrategies.get(PropertiesKey.getEnumByKey(key)).parse(value, properties);
+        }
+
+        return properties;
+    }
+
+    private Properties parse(Properties properties, String line) {
+        final String [] values = line.split("=");
+        final String key = values[0];
+        final String value = values[1];
+
+        final PropertiesKey keyEnum = PropertiesKey.getEnumByKey(key);
+
+        switch (keyEnum) {
+            case BASE_URL:
+                properties.setBaseUrl(value);
+                return properties;
+
+            case TIMEOUT:
+                properties.setTimeout(Integer.valueOf(value));
+                return properties;
+
+            case PORT:
+                properties.setPort(Integer.valueOf(value));
+                return properties;
+
+            case ANY:
+                properties.setAny(value);
+                return properties;
+
+            case ANY:
+                properties.setAny(value);
+                return properties;
+
+            case ANY:
+                properties.setAny(value);
+                return properties;
+
+            case ANY:
+                properties.setAny(value);
+                return properties;
+
+            case ANY:
+                properties.setAny(value);
+                return properties;
+
+            case ANY:
+                properties.setAny(value);
+                return properties;
+
+            case ANY:
+                properties.setAny(value);
+                return properties;
+
+            case ANY:
+                properties.setAny(value);
+                return properties;
+
+            case ANY:
+                properties.setAny(value);
+                return properties;
         }
 
         return properties;
